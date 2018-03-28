428a08d7d9f12d785339e5714db42b437427278c
silk <silk@khel.dar>
2018-03-27 17:03:01 -0700

    add STRIDE constant

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use std::path::Path;
 
 
 fn main() {
+    // size of each pixel when rendered. Defaults to 1, increase if you want to zoom in.
     const BLOCK_SIZE: i32 = 1;
+    // we're dealing with rgb triplets, so that's our stride.
+    const STRIDE: i32 = 3;
 
     let args: Vec<String> = env::args().collect();
 
@@ -57,7 +60,7 @@ fn main() {
         }
     }
 
-    assert!(pixels.len() as i32 == num_cols * num_rows * 3, "Length of pixel array must match num_cols * num_rows * 3. Input is malformed!");
+    assert!(pixels.len() as i32 == num_cols * num_rows * STRIDE, "Length of pixel array must match num_cols * num_rows * {}. Input is malformed!", STRIDE);
 
     println!("num_cols: {} num_rows: {}", num_cols, num_rows);
     let window_dimensions = [(num_cols * BLOCK_SIZE) as u32, (num_rows * BLOCK_SIZE) as u32];
@@ -71,9 +74,9 @@ fn main() {
             for row in 0..num_rows {
                 for col in 0..num_cols {
                     // TODO: add column_stride constant
-                    let red_component = pixels[(num_cols * row * 3 + col * 3) as usize] as f32;
-                    let green_component = pixels[(num_cols * row * 3 + col * 3 + 1) as usize] as f32;
-                    let blue_component = pixels[(num_cols * row * 3 + col * 3 + 2) as usize] as f32;
+                    let red_component = pixels[(num_cols * row * STRIDE + col * STRIDE) as usize] as f32;
+                    let green_component = pixels[(num_cols * row * STRIDE + col * STRIDE + 1) as usize] as f32;
+                    let blue_component = pixels[(num_cols * row * STRIDE + col * STRIDE + 2) as usize] as f32;
                     rectangle([red_component/max_val, green_component/max_val, blue_component/max_val, 1.0],
                         [(col * BLOCK_SIZE) as f64, (row * BLOCK_SIZE) as f64, BLOCK_SIZE as f64, BLOCK_SIZE as f64],
                         context.transform,
