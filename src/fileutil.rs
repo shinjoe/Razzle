0e2bc822569eb3d0b92098a58bf69b692339f90e
silk <silk@khel.dar>
2018-03-28 18:14:36 -0700

    Begone Piston! Leaks memory like a sieve. Now using rust-sdl2, which works great

--- src/fileutil.rs
+++ src/fileutil.rs
@@ -4,8 +4,13 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-pub fn parse_file() -> (i32, i32, Vec<f32>) {
+pub fn parse_file() -> (u32, u32, Vec<u32>) {
+    const SUPPORTED_MAGIC_NUMBER: &str = "P3";
+
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Need to supply name of file as command line argument.");
+    }
     let path = Path::new(&args[1]);
     let display = path.display();
 
@@ -21,30 +26,32 @@ pub fn parse_file() -> (i32, i32, Vec<f32>) {
     }
 
     let lines = s.split("\n");
-    let mut num_rows = -1;
-    let mut num_cols = -1;
-    let mut max_val = -1.0;
-    let mut pixels: Vec<f32> = Vec::new();
+    let mut num_rows: u32 = 0;
+    let mut num_cols: u32 = 0;
+    let mut pixels: Vec<u32> = Vec::new();
     for (i, line) in lines.enumerate() {
         match i {
             0 => {
-                if line.trim() != "P3" {
+                if line.trim() != SUPPORTED_MAGIC_NUMBER {
                     panic!("Unsupported magic number: {}", line)
                 }
             },
             1 => {
-                let dimensions: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
+                let dimensions: Vec<u32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
                 num_cols = dimensions[0];
                 num_rows = dimensions[1];
             },
             2 => {
-                max_val = line.trim().parse().unwrap();
+                let max_val: u32 = line.trim().parse().unwrap();
+                if max_val != 255 {
+                    panic!("Unexpected max pixel color value: {}", max_val);
+                }
             }
             _ => {
                 let cur_line_of_pixels = line.split_whitespace();
                 for x in cur_line_of_pixels {
-                    let color_value: i32 = x.parse().unwrap();
-                    pixels.push(color_value as f32/max_val)
+                    let color_value: u32 = x.parse().unwrap();
+                    pixels.push(color_value)
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,66 @@
-extern crate piston_window;
+extern crate sdl2;
 
-use piston_window::*;
+use sdl2::event::Event;
+use sdl2::rect::Rect;
+use sdl2::keyboard::Keycode;
 
 mod fileutil;
 
+#[allow(unused_must_use)]
+
 fn main() {
     // size of each pixel when rendered. Defaults to 1, increase if you want to zoom in.
-    const BLOCK_SIZE: i32 = 1;
+    const BLOCK_SIZE: u32 = 1;
     // we're dealing with rgb triplets, so that's our stride.
-    const STRIDE: i32 = 3;
+    const STRIDE: u32 = 3;
 
     let (num_rows, num_cols, pixels) = fileutil::parse_file();
 
-    assert!(pixels.len() as i32 == num_cols * num_rows * STRIDE, "Length of pixel array must match num_cols * num_rows * {}. Input is malformed!", STRIDE);
+    assert!(pixels.len() == (num_cols * num_rows * STRIDE) as usize, "Length of pixel array must match num_cols * num_rows * {}. Input is malformed!", STRIDE);
 
-    println!("num_cols: {} num_rows: {}", num_cols, num_rows);
-    let window_dimensions = [(num_cols * BLOCK_SIZE) as u32, (num_rows * BLOCK_SIZE) as u32];
-    println!("width: {} height: {}", window_dimensions[0], window_dimensions[1]);
-    let mut window: PistonWindow = WindowSettings::new("PPM Viewer", window_dimensions)
-                                   .exit_on_esc(true).build().unwrap();
+    let window_dimensions = [num_cols * BLOCK_SIZE, num_rows * BLOCK_SIZE];
+    println!("{} x {}", window_dimensions[0], window_dimensions[1]);
 
-    const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+    let sdl_context = sdl2::init().unwrap();
+    let video_context = sdl_context.video().unwrap();
 
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear(WHITE, graphics);
-            for row in 0..num_rows {
-                for col in 0..num_cols {
-                    let index = (num_cols * row * STRIDE + col * STRIDE) as usize;
-                    rectangle([pixels[index], pixels[index + 1], pixels[index + 2], 1.0],
-                        [col as f64 , row as f64, BLOCK_SIZE as f64, BLOCK_SIZE as f64],
-                        context.transform,
-                        graphics);
+    let window = match video_context.window("Razzle", window_dimensions[0], window_dimensions[1]).position_centered().opengl().build() {
+        Ok(window) => window,
+        Err(err) => panic!("failed to create window: {}", err)
+    };
+
+    let mut canvas = match window.into_canvas().build() {
+        Ok(canvas) => canvas,
+        Err(err) => panic!("failed to create canvas: {}", err)
+    };
+
+    canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
+    canvas.clear();
+
+    for row in 0..num_rows {
+        for col in 0..num_cols {
+            let index = (num_cols * row * STRIDE + col * STRIDE) as usize;
+            canvas.set_draw_color(sdl2::pixels::Color::RGB(pixels[index] as u8, pixels[index + 1] as u8, pixels[index + 2] as u8));
+            canvas.fill_rect(Rect::new(col as i32, row as i32, BLOCK_SIZE, BLOCK_SIZE));
+        }
+    }
+
+    canvas.present();
+
+    // todo: limit fps
+    let mut events = sdl_context.event_pump().unwrap();
+    let mut done = false;
+    while !done {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit{..} => done = true,
+                Event::KeyDown {keycode: Some(keycode), ..} => {
+                    if keycode == Keycode::Escape {
+                        done = true
+                    }
                 }
+                _ => {}
             }
-        });
+        }
     }
 }
